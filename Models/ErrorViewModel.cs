c16fb1c0f9c3236def23088e453998ad780539e2
somes56 <somesjaya_56@yahoo.com>
2023-02-08 22:07:01 +0800

    Add Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Auth0Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
